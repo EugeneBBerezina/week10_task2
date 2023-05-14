87173f94af15970119b8d83b371c8eeb59f29164
EugeneBBerezina <hysteriajane@gmail.com>
2023-05-14 15:21:22 +0300

    add all info

---
+++ index.js
@@ -0,0 +1,9 @@
+let img = document.getElementById('elem-img');
+
+function showPrevious() {
+    img.src = 'assets/images/foto2.jpg'
+}
+
+function showNext() {
+    img.src = 'assets/images/foto3.jpg'
+}
